1a07f2960ffc74515dbed6e43b112f6e57b7c42c
Gödri Szabolcs <godri04@gmail.com>
2025-08-01 23:57:57 +0200

    Új logo, visza kerult az about page, contact fejlesztés, és minden oldal elejére oldal cime resze

--- src/app/[locale]/(marketing)/contact/page.tsx
+++ src/app/[locale]/(marketing)/contact/page.tsx
@@ -2,12 +2,12 @@ import { getTranslations } from 'next-intl/server';
 import ContactForm from './contactForm';
 
 type Props = {
-  params: { locale: string };
+  params: Promise<{ locale: string }>;
 };
 
 export async function generateMetadata({ params }: Props) {
   const t = await getTranslations({
-    locale: params.locale,
+    locale: (await params).locale,
     namespace: 'Contact',
   });
 
